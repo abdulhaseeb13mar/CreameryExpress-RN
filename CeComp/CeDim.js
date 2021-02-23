c0777b0217be83b3a9cde00e03eaa521d58bac37
abdulhaseeb13mar <abdulhaseeb13mar@gmail.com>
2021-02-23 07:09:51 +0500

    app done

--- CeComp/CeDim.js
+++ CeComp/CeDim.js
@@ -1,18 +1,7 @@
 import {Dimensions} from 'react-native';
-// import SafeArea from 'react-native-safe-area';
 
 const width = Dimensions.get('window').width;
 const height = Dimensions.get('window').height;
-// const height = Dimensions.get('window').height - StatusBar.currentHeight;
-// export const get_dimensions = async () => {
-//   const insets = await SafeArea.getSafeAreaInsetsForRootView().then(
-//     (res) => res.safeAreaInsets,
-//   );
-//   return {
-//     width: width - (insets.left + insets.right),
-//     height: height - (insets.top + insets.bottom),
-//   };
-// };
 
 export const H_W = {
   width: width,
